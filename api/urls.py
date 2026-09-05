2a6b696dfe27b8e424c78e5fd9174309556c4e7f
Akshay H <akshayhuliyaru2005@gmail.com>
2026-09-05 18:56:07 +0530

    learnt how to do dynamic filtering by overriding get_queryset() method

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     path('products/<int:product_id>/',views.ProductDetailAPIView.as_view()),
     path('orders/',views.OrderListAPIView.as_view()),
     path('product/info/',views.product_info),
-    path('update_product/<int:pk>/',views.update_product)
+    path('update_product/<int:pk>/',views.update_product),
+    path('user/orders/',views.UserOrderListAPIView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -20,6 +20,13 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
 class OrderListAPIView(generics.ListAPIView):
     queryset=Order.objects.prefetch_related('items__product')
     serializer_class=OrderSerializer
+class UserOrderListAPIView(generics.ListAPIView):
+    queryset=Order.objects.prefetch_related('items__product')
+    serializer_class=OrderSerializer
+    def get_queryset(self):
+        user = self.request.user
+        query=super().get_queryset()
+        return query.filter(user=user)
 @api_view(['GET'])
 def product_info(request):
     products=Product.objects.all()
