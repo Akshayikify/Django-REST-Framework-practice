78cb8e518ee8861b47f26ae604ad59e7a5f89393
Akshay H <akshayhuliyaru2005@gmail.com>
2026-09-08 22:22:50 +0530

    Learnt about post request through APIView

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,6 @@ urlpatterns=[
     path('orders/',views.OrderListAPIView.as_view()),
     path('product/info/',views.ProductInfoAPIView.as_view()),
     path('update_product/<int:pk>/',views.UpdateDeleteAPIView.as_view()),
-    path('user/orders/',views.UserOrderListAPIView.as_view())
+    path('create_product/',views.CreateProductAPIView.as_view()),
+    path('user/orders/',views.UserOrderListAPIView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -48,9 +48,16 @@ class UpdateDeleteAPIView(APIView):
             serializer.save()
             return Response(serializer.data,status=status.HTTP_200_OK)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    def delete(self,request,*args,**kwargs):
+    def delete(self,request,*args,**kwargs):                                                    
         product=get_object_or_404(pk=args[0])
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-        
+class CreateProductAPIView(APIView):
+    def post(self,request,*args,**kwargs):
+        serializer=ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            
