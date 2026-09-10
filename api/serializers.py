15f680da2e7206efc3d59a85352d51c20294c6c0
Akshay H <akshayhuliyaru2005@gmail.com>
2026-09-10 21:15:21 +0530

    revisiting serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,49 +1,27 @@
+#Import serializers from rest_framework package
 from rest_framework import serializers
+#Import all model classes from models module
 from .models import Product,Order,OrderItem
-class ProductSerializer(serializers.ModelSerializer):
+#Create a Product Serializer --> Json 
+class ProductSerializer(serializers.Serializer):
     class Meta:
         model=Product
-        fields=[
-            "id",
-            'name',
-            'price',
-            'stock'
-        ]
-    def validate_price(self,val):
-        if val<=0:
-            """
-            This is used to validate the price of the product
-            """
-            raise serializers.ValidationError("The price should be greater than 0")
-        return val
-    
+        fields=['id','name','price','stock']
+        #Define a method that is used for the validation of the price
+        def valid_price(self,val):
+            if val<=0:
+                serializers.ValidationError("The price should not be negative or zero")
+            return self.val
+#Create a OrderItemSerializer class
 class OrderItemSerializer(serializers.ModelSerializer):
-    product_name=serializers.CharField(max_length=200,source='product.name')
-    product_price=serializers.DecimalField(max_digits=10,decimal_places=2,source='product.price')
     class Meta:
         model=OrderItem
         fields=[
-            'product_name',
-            'product_price',
-            'quantity'
+           'product','order','quantity'
         ]
 class OrderSerializer(serializers.ModelSerializer):
-    items=OrderItemSerializer(many=True,read_only=True)
-    total_price=serializers.SerializerMethodField()
-    def get_total_price(self,obj):
-        price=[item.item_subtotal for item in obj.items.all()]
-        return sum(price)
+    items=OrderItemSerializer()
     class Meta:
         model=Order
-        fields=[
-            'order_id',
-            'user',
-            'created_at',
-            'status',
-            'items',
-            'total_price'
-        ]
-class ProductInfoSerializer(serializers.Serializer):
-    products=ProductSerializer(many=True)
-    count=serializers.IntegerField()
-    max_price=serializers.FloatField()
+        fields=['order_id','user','created_at']
+        
