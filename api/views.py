aaade0ae6a7aef90b60d82ff52e6fa970f79e9c5
Akshay H <akshayhuliyaru2005@gmail.com>
2026-09-24 22:04:52 +0530

    learnt to create mixins

--- api/views.py
+++ api/views.py
@@ -12,50 +12,72 @@ from rest_framework.views import APIView
 from api.filters import ProductFilter
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import mixins
 # Creating get and post requests
-class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset=Product.objects.all()
-    serializer_class=ProductSerializer
-    filterset_class=(ProductFilter)
-    filter_backends = [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
-    search_fields=['=name','description']
-    ordering_fields=['name','price']
-    def get_permissions(self):
-        self.permission_classes=[AllowAny]
-        if self.request.method=='POST':
-            self.permission_classes=[IsAdminUser]
-        return super().get_permissions()
+# class ProductListCreateAPIView(generics.ListCreateAPIView):
+#     queryset=Product.objects.all()
+#     serializer_class=ProductSerializer
+#     filterset_class=(ProductFilter)
+#     filter_backends = [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
+#     search_fields=['name','description']
+#     ordering_fields=['name','price']
+#     def get_permissions(self):
+#         self.permission_classes=[AllowAny]
+#         if self.request.method=='POST':
+#             self.permission_classes=[IsAdminUser]
+#         return super().get_permissions()
  
-class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+# class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset=Product.objects.all()
+#     serializer_class=ProductSerializer
+#     lookup_url_kwarg='product_id'
+#     def get_permissions(self):
+#         self.permission_classes=[AllowAny]
+#         if self.request.method in ['PUT','DELETE','PATCH']:
+#             self.permission_classes=[IsAdminUser]
+#         return super().get_permissions()
+
+# class OrderListAPIView(generics.ListAPIView):
+#     queryset=Order.objects.prefetch_related('items__product').all()
+#     serializer_class=OrderSerializer
+
+# class UserOrderListAPIView(generics.ListAPIView):
+#     queryset=Order.objects.prefetch_related('items__product').all()
+#     serializer_class=OrderSerializer
+#     permission_classes=[IsAuthenticated]
+#     def get_queryset(self):
+#         user = self.request.user
+#         qs=super().get_queryset()
+#         return qs.filter(user=user)
+# class ProductInfo(APIView):
+#     permission_classes=[IsAuthenticated]
+#     def get(self,request):
+#         products=Product.objects.all()
+#         serializer=ProductInfoSerializer({
+#             'products': products,
+#             'count': len(products),
+#             'max_price': products.aggregate(max_price=Max('price'))['max_price']
+#         })
+#         return Response(serializer.data)
+
+# mixins  in django rest framework
+
+class ProductListCreateAPIView(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
-    lookup_url_kwarg='product_id'
-    def get_permissions(self):
-        self.permission_classes=[AllowAny]
-        if self.request.method in ['PUT','DELETE','PATCH']:
-            self.permission_classes=[IsAdminUser]
-        return super().get_permissions()
-
-class OrderListAPIView(generics.ListAPIView):
-    queryset=Order.objects.prefetch_related('items__product').all()
-    serializer_class=OrderSerializer
-
-class UserOrderListAPIView(generics.ListAPIView):
-    queryset=Order.objects.prefetch_related('items__product').all()
-    serializer_class=OrderSerializer
-    permission_classes=[IsAuthenticated]
-    def get_queryset(self):
-        user = self.request.user
-        qs=super().get_queryset()
-        return qs.filter(user=user)
-class ProductInfo(APIView):
-    permission_classes=[IsAuthenticated]
-    def get(self,request):
-        products=Product.objects.all()
-        serializer=ProductInfoSerializer({
-            'products': products,
-            'count': len(products),
-            'max_price': products.aggregate(max_price=Max('price'))['max_price']
-        })
-        return Response(serializer.data)
-        
+    
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
+class ProductUpdateAPIView(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+    def get(self,request,*args,**kwargs):
+        return self.retrieve(request,*args,**kwargs)
+    def put(self,request,*args,**kwargs):
+        return self.update(request,*args,**kwargs)
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+    def delete(self,request,*args,**kwargs):
+        return self.destroy(request,*args,**kwargs)
--- drf_main/settings.py
+++ drf_main/settings.py
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
